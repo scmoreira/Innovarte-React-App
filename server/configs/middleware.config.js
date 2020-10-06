c8e82ed83f8e40ee10319b815931d58767c5629f
scmoreira <silvanamoreira85@outlook.com>
2020-10-06 11:59:08 +0200

    first server deploy on heroku app

--- server/configs/middleware.config.js
+++ server/configs/middleware.config.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
 
-const whitelist = 'http://localhost:3000'
+const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL]
 const corsOptions = {
     origin: (origin, cb) => {
         const originIsWhitelisted = whitelist.includes(origin)
