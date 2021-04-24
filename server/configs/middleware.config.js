bacd1cb14fab4af713b24981f364bb39a94044ee
scmoreira <silvanamoreira85@outlook.com>
2021-04-24 13:33:07 +0200

    delete cors options

--- server/configs/middleware.config.js
+++ server/configs/middleware.config.js
@@ -13,7 +13,7 @@ const corsOptions = {
 }
 
 module.exports = app => {
-    app.use(cors(corsOptions))
+    app.use(cors())
     app.use(logger('dev'))
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
